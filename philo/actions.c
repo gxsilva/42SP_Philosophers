ff93cec9b2a897ce41966fa1c2567ea5ee8f0cb7
Lucas Gabriel Silva Xavier <lsilva-x@c1r4p2.42sp.org.br>
2025-03-10 17:00:47 -0300

    Feature: Norminette | Fix: Monitor

--- philo/actions.c
+++ philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 14:51:03 by lsilva-x          #+#    #+#             */
-/*   Updated: 2025/03/10 15:43:52 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/03/10 16:01:04 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,26 @@
 
 static void	take_forks(t_philo *philo)
 {
-	pthread_mutex_lock(philo->l_fork);
-	message(TAKE_FORK, philo);
-	pthread_mutex_lock(philo->r_fork);
-	message(TAKE_FORK, philo);
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->l_fork);
+		message(TAKE_FORK, philo);
+		pthread_mutex_lock(philo->r_fork);
+		message(TAKE_FORK, philo);
+	}
+	else
+	{
+		pthread_mutex_lock(philo->r_fork);
+		message(TAKE_FORK, philo);
+		pthread_mutex_lock(philo->l_fork);
+		message(TAKE_FORK, philo);
+	}
 }
 
 static void	drop_forks(t_philo *philo)
 {
-	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
+	pthread_mutex_unlock(philo->l_fork);
 	message(DROP_FORK, philo);
 	ft_usleep(philo->data->sleep_time, philo);
 }
