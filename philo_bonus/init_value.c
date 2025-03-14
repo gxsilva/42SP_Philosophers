3a155b6963c41a527d5d64a0c6b9dfb65f4b3480
Lucas Gabriel Silva Xavier <lsilva-x@c1r4p1.42sp.org.br>
2025-03-13 21:23:41 -0300

    Feature: Initial change from threads to processes and from mutexes to semaphores

---
+++ philo_bonus/init_value.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_value.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 14:34:04 by lsilva-x          #+#    #+#             */
+/*   Updated: 2025/03/13 20:56:50 by lsilva-x         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+void		init_value(char **argv, int argc, t_data *philo_s);
+static void	alloc_data(t_data *philo_s);
+static void	init_forks(t_data *philo_s);
+static void	init_philo(t_data *philo_s);
+
+static void	init_data(char **argv, int argc, t_data *philo_s)
+{
+	int		n_philo;
+
+	n_philo = ft_atoi(argv[1]);
+	if (n_philo <= 0 || n_philo > 200)
+	{
+		free(philo_s);
+		terminate_with_error(PHI_OUT_RANGE, -1);
+	}
+	philo_s->philo_num = n_philo;
+	philo_s->dead = 0;
+	philo_s->death_time = ft_atoi(argv[2]);
+	philo_s->eat_time = ft_atoi(argv[3]);
+	philo_s->sleep_time = ft_atoi(argv[4]);
+	if (argc == 6)
+		philo_s->meals_nb = ft_atoi(argv[5]);
+	else
+		philo_s->meals_nb = -1;
+	// pthread_mutex_init(&philo_s->write, NULL);
+	// pthread_mutex_init(&philo_s->lock, NULL);
+}
+
+static void	alloc_data(t_data *philo_s)
+{
+	philo_s->pid = (pid_t *)malloc(sizeof(pid_t) * philo_s->philo_num);
+	if (!philo_s->pid)
+	{
+		free_philo(philo_s);
+		terminate_with_error(FAILED_MALLOC, -2);
+	}
+	philo_s->forks = malloc(sizeof(sem_t) * philo_s->philo_num);
+	if (!philo_s->forks)
+	{
+		free_philo(philo_s);
+		terminate_with_error(FAILED_MALLOC, -2);
+	}
+	philo_s->philos = malloc(sizeof(t_philo) * philo_s->philo_num);
+	if (!philo_s->philos)
+	{
+		free_philo(philo_s);
+		terminate_with_error(FAILED_MALLOC, -2);
+	}
+}
+
+static void	init_forks(t_data *philo_s)
+{
+	sem_unlink(SEM_FORKS);
+	sem_unlink(SEM_LOCKS);
+	sem_unlink(SEM_WRITE);
+	sem_unlink(SEM_PH_LOCK);
+	philo_s->forks = sem_open(SEM_FORKS, O_CREAT | O_EXCL, 0644, philo_s->philo_num);
+	philo_s->lock = sem_open(SEM_FORKS, O_CREAT | O_EXCL, 0644, 1);
+	philo_s->write = sem_open(SEM_FORKS, O_CREAT | O_EXCL, 0644, 1);
+	philo_s->ph_lock = sem_open(SEM_FORKS, O_CREAT | O_EXCL, 0644, 1);
+}
+
+static void	init_philo(t_data *philo_s)
+{
+	int		i;
+
+	i = 0;
+	while (i < philo_s->philo_num)
+	{
+		philo_s->philos[i].data = philo_s;
+		// philo_s->philos[i].id = i + 1; -> it must be set after
+		philo_s->philos[i].eat_cont = 0;
+		philo_s->philos[i].status = 0;
+		philo_s->philos[i].eating = 0;
+		philo_s->philos[i].time_to_die = philo_s->death_time;
+		// pthread_mutex_init(&philo_s->philos[i].lock, NULL);
+		i++;
+	}
+}
+
+void	init_value(char **argv, int argc, t_data *philo_s)
+{
+	init_data(argv, argc, philo_s);
+	alloc_data(philo_s);
+	init_forks(philo_s);
+	init_philo(philo_s);
+}
